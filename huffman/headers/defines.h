ab03c163d88d2946af5640e89455413ed5e25a7d
Kraken32bin <kraken32bin@protonmail.com>
2020-01-08 05:37:03 -0300

    Log error message macro

--- huffman/headers/defines.h
+++ huffman/headers/defines.h
@@ -2,3 +2,9 @@
 #define true 1
 
 #define nullptr NULL
+
+#define logerr(msg) {\
+    int line = __LINE__ - 3;\
+    printf("\x1b[94m%s:%i\x1b[0m$ @%s > %s: \x1b[31m%s\n\x1b[0m",\
+    __FILE__, line, __func__, msg, strerror(errno));\
+}
