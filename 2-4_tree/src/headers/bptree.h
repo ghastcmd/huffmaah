0c013c823360b9b22441db252e34aec901ee234c
Kraken32bin <kraken32bin@protonmail.com>
2020-02-08 17:31:26 -0300

    Fully fledged b+ tree implementation

--- 2-4_tree/src/headers/bptree.h
+++ 2-4_tree/src/headers/bptree.h
@@ -3,6 +3,11 @@
 
 #define BPSIZE 4
 
+#define bpnode_print(node) if (node) {\
+    bpnode_print_current(node);\
+    puts("");\
+}
+
 typedef struct bpnode
 {
     struct bpnode* parent;
@@ -19,6 +24,8 @@ void bpnode_print_current(bpnode* node);
 
 void bpnode_print_tree(bpnode* node);
 
+void bpnode_get_qnt(bpnode* node, int* qnt);
+
 void bpnode_childs_current(bpnode* node);
 
 void free_bpnode(bpnode* root);
@@ -27,4 +34,4 @@ bool bpnode_search(bpnode* node, int val);
 
 bpnode* bpnode_find_node(bpnode* node, int val);
 
-void bpnode_remove(bpnode* root, int val);
+void bpnode_remove(bpnode** root, int val);
