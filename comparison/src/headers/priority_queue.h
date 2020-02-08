6ceb8554e2a58e4250667fff7184223e9fa95612
Viihtorugo <victor-torugo@hotmail.com>
2020-02-08 11:23:04 -0300

    [comparison-01]: added the priority queue struct without heap]

---
+++ comparison/src/headers/priority_queue.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "pch.h"
+
+struct node {
+	int item;
+	int priority;
+	struct node *next;
+};
+
+struct priority_queue {
+	struct node *head;
+};
+
+//function to verify if head of the struct node is null
+int is_empty_node (struct node *node);
+
+//alloc memory for to struct priority_queue (create struct)
+struct priority_queue *create_priority_queue ();
+
+//add one node on struct with priority
+void enqueue (struct priority_queue *pq, int i, int p);
+
+//remove the primary node of the head
+struct node *dequeue (struct priority_queue *pq);
---
+++ comparison/src/priority_queue.c
@@ -0,0 +1,68 @@
+#include "pch.c"
+
+int is_empty_node (struct node *node)
+{
+	return (node == NULL);
+}
+
+struct priority_queue *create_priority_queue ()
+{
+	struct priority_queue *pq = (struct priority_queue *) malloc (sizeof(struct priority_queue));
+
+	pq->head == NULL;
+
+	if (pq == NULL)
+	{
+		printf("Memory overflow\n");
+		return NULL;
+	}
+
+	return pq;
+}
+
+void enqueue (struct priority_queue *pq, int i, int p)
+{
+	struct node *new_node = (struct node *) malloc(sizeof(struct node));
+
+	new_node->item = i;
+	new_node->priority = p;
+	new_node->next = NULL;
+
+	if (is_empty_node(pq->head) || p > pq->head->priority)
+	{
+		new_node->next = pq->head;
+		pq->head = new_node;
+	}
+	else
+	{
+		struct node *current = pq->head;
+		struct node *previous = current;
+
+		while (current != NULL && p <= current->priority)
+		{
+			previous = current;
+			current = current->next;
+		}
+		
+		new_node->next = current;
+		previous->next = new_node; 
+	}
+}
+
+struct node *dequeue (struct priority_queue *pq)
+{
+	if (is_empty_node(pq->head))
+	{
+		printf("Priority queue underflow\n");
+		return NULL;
+	}
+	else
+	{
+		struct node *dequeued = pq->head;
+
+		pq->head = pq->head->next;
+		dequeued->next = NULL;
+
+		return dequeued;
+	}
+}
