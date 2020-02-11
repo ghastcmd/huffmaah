b49f593f363e63b680a52f5f378095cf8e48de14
Kraken32bin <kraken32bin@protonmail.com>
2020-02-10 22:50:34 -0300

    Plotting capabilities

---
+++ comparison/plot.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+import csv
+
+data_folder = 'data/'
+
+without_heap = 'priority_queue_without_heap.txt'
+with_heap = 'priority_queue_with_heap.txt'
+
+with open(f'{data_folder + without_heap}', 'r') as heap_file:
+    heap_data = csv.reader(heap_file)
+    number, comparison = zip(*list(heap_data))
+
+    number = list(map(int, number))
+    comparison = list(map(int, comparison))
+
+    plt.scatter(number, comparison, label='With heap')
+
+
+with open(f'{data_folder + with_heap}', 'r') as queue_file:
+    queue_data = csv.reader(queue_file)
+    number, comparison = zip(*list(queue_data))
+
+    number = list(map(int, number))
+    comparison = list(map(int, comparison))
+
+    plt.scatter(number, comparison, label='Without heap')
+
+plt.ylabel('Number of comparisons')
+plt.xlabel('Draft numbers')
+plt.title('Priority queue with heap vs. without heap')
+plt.show()
